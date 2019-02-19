4c4ff0067a83f098bbd01354cefd79391e5c8f50
Cesar Alvarez <ing.cesaralvarezg@gmail.com>
2019-02-19 09:58:44 -0500

    Recibir orden del teclado

--- robotfubolista.ino
+++ robotfubolista.ino
@@ -1,3 +1,5 @@
+//REcibir orden del teclado
+
 #include<Servo.h>
 #define PINSERVODERECHO 12//pIN DEL SERVO DERECHO
 
@@ -6,17 +8,30 @@ Servo servoDerecho;//Declaro el servo derecho
 
 
 void setup() {
+  Serial.begin(9600);
   servoDerecho.attach(PINSERVODERECHO);//Indicar que esta conectado al pin SERVODERECHO
 }
 
 
 void loop() {
-  servoDerecho.write(0);//Girar en sentido horario
-  delay(1000);
-  servoDerecho.write(90);//Detenerse
-  delay(1000);
-  servoDerecho.write(180);//Girar en sentido antihorario
-  delay(1000);
-  servoDerecho.write(90);//Detenerse
-  delay(1000);
+  char orden;
+  if(Serial.available()>0)
+  {
+    orden=Serial.read();
+    if(orden=='w')
+      {
+        servoDerecho.write(0);//Girar en sentido horario
+        delay(1000);
+        servoDerecho.write(90);//Detenerse
+        delay(1000);
+        servoDerecho.write(180);//Girar en sentido antihorario
+        delay(1000);
+        servoDerecho.write(90);//Detenerse
+        delay(1000);      
+      }else{
+            servoDerecho.write(90);//Detenerse
+        }
+  }
+  
+  
  }
